e5c0625ad13f989ddd5bdeae71ab5b80daa953b8
ernsimsek <ali.erenn.simsek@gmail.com>
2026-05-16 00:45:20 +0300

    Footer

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,8 @@
+import AppPromoBanner from "../components/Home/AppPromoBanner"
 import Features from "../components/Home/Features"
 import Hero from "../components/Home/Hero"
 import HomeCategories from "../components/Home/HomeCategories"
+import Newsletter from "../components/Home/Newsletter"
 import PopularProducts from "../components/Home/PopularProducts"
 
 
@@ -11,6 +13,8 @@ const Home = () => {
       <Features />
       <HomeCategories />
       <PopularProducts />
+      <AppPromoBanner />
+      <Newsletter />
     </div>
   )
 }
