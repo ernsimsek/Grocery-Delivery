a2807aee796d5168458e7a5e1ce25258f038bc6e
erensimsek <ali.erenn.simsek@gmail.com>
2026-06-04 19:48:01 +0300

    vercel

--- server/controllers/corderController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-// Create order
-// POST /api/orders
-export const createOrder = async (req: Request, res: Response) => {
-    const { items, shippingAddress, paymentMethod } = req.body;
-}
--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,10 @@ import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 
@@ -17,9 +21,11 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 
-app.use("/api/auth", authRouter);
-app.use("/api/products", productRouter);
-app.use("/api/upload", uploadRouter);
+app.use("/api/auth", authRouter)
+app.use("/api/products", productRouter)
+app.use("/api/upload", uploadRouter)
+app.use("/api/orders", orderRouter)
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // Error handling
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
